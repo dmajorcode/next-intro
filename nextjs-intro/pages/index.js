717fd13a5e6e5cfb4c1d8c7d1a98f1695ff51e20
dmajorcode <91581474+dmajorcode@users.noreply.github.com>
2022-09-19 19:52:51 +0900

    first push on Next js intro storage

---
+++ nextjs-intro/pages/index.js
@@ -0,0 +1,25 @@
+import Head from "next/head";
+import Seo from "../components/Seo";
+import { useState, useEffect } from "react";
+
+export default function Home() {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const { results } = await (await fetch(`/api/movies`)).json();
+      setMovies(results);
+    })();
+  }, []);
+  return (
+    <div>
+      <Seo title="Home" />
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie) => (
+        <div key={movie.id}>
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
+    </div>
+  );
+}
